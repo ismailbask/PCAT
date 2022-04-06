a9e5863ec5cc4b22653621b3c81edaaa39a9b7a2
İsmail Basık <87307720+ismailbask@users.noreply.github.com>
2022-04-06 15:37:29 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const fn = function () {
-    console.log("selamlar");
-};
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static('public'))
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`Server ${PORT} portunda başlatıldı`);
+})
